fd4d5b96df777f9de9f07962c942bb58a3c1fa93
sameerasw <sameerasw.com@gmail.com>
2025-07-26 05:48:04 +0530

    Initial Commit

---
+++ weather-app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather-app
+//
+//  Created by Sameera Sandakelum on 2025-07-26.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
